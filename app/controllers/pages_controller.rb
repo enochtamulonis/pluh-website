11d8c87ec4dfacc66e71069556100e7abd95eeb3
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-08 19:41:18 -0500

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+    @songs = [
+      "PLUH INTRO!",
+      "pluhgang",
+      "$TAR$HYNIN w/ teardrop$teez",
+      "midnight smoking w/ luc3y",
+      "based",
+      "online shawty",
+      "penjamin city w/ dieyowai",
+      "pluh freestyle w/ indigo",
+      "YUNNG KIITTY LEGEND"
+    ]
+  end
+end
